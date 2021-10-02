35708003a16a69a3a48459b69c46a52c7291a8e2
igwubor <igwubor@outlook.com>
2021-10-02 13:37:22 +0100

    addedReactIcons

--- src/Divs.js
+++ src/Divs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { FiPieChart } from 'react-icons/fi';
 function Divs() {
     return (
         <div  className='wrapper'>
@@ -33,10 +33,60 @@ function Divs() {
        
         </div>
          <div className='parentloans'>
-         <div className='IconsDiv'>Icons</div>
+         <div className='IconsDiv'><FiPieChart className='fipieChart' /></div>
          <div className='loans'>Loans</div>
-         <div className='newbutton'>New</div>
+         <div className='newbutton'>
+             <div className='buttonText'>New</div>
+         </div>
      </div>
+     <div className='divlineAbove'>
+
+    
+     <div className='parent parentThree'>
+            <div className='child childOne'></div>
+            <div className='child childTwo'>
+            <div className='lineDiv'></div>
+            </div>
+        </div>
+        <div className='parent parentFour'>
+            <div className='child childOne'></div>
+            <div className='child childTwo'>
+            <div className='lineDiv'></div>
+            </div>
+        </div>
+
+        <div className='parent parentThree'>
+            <div className='child childOne'></div>
+            <div className='child childTwo'>
+            <div className='lineDiv'></div>
+            </div>
+        </div>
+        <div className='parent parentFour'>
+            <div className='child childOne'></div>
+            <div className='child childTwo'>
+            <div className='lineDiv'></div>
+            </div>
+        </div>
+        <div className='parent parentFour'>
+            <div className='child childOne'></div>
+            <div className='child childTwo'>
+            <div className='lineDiv'></div>
+            </div>
+        </div>
+
+        <div className='parent parentThree'>
+            <div className='child childOne'></div>
+            <div className='child childTwo'>
+            <div className='lineDiv'></div>
+            </div>
+        </div>
+        <div className='parent parentFour'>
+            <div className='child childOne'></div>
+            <div className='child childTwo'>
+            <div className='lineDiv'></div>
+            </div>
+        </div>
+        </div>
      </div>
         
         
